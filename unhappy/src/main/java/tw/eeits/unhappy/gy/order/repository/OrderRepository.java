b4d3c9e0fcca66e6796c3f1f3df079dd360bdec1
David227-MSI 😎 <a2272485@gmail.com>
2025-04-24 22:46:52 +0800

    refactor(repository): 移除未使用的 Query

--- unhappy/src/main/java/tw/eeits/unhappy/gy/order/repository/OrderRepository.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/order/repository/OrderRepository.java
@@ -2,7 +2,6 @@ package tw.eeits.unhappy.gy.order.repository;
 
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tw.eeits.unhappy.gy.domain.Order;
