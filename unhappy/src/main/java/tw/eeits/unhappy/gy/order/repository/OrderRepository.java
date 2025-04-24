bd7625c7fabceff497ea962d3942e74e5a1ebaff
David227-MSI 😎 <a2272485@gmail.com>
2025-04-24 22:40:03 +0800

    feat(repository): 新增查詢訂單明細及會員所有訂單的方法，並解決 LAZY loading 問題

--- unhappy/src/main/java/tw/eeits/unhappy/gy/order/repository/OrderRepository.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/order/repository/OrderRepository.java
@@ -1,7 +1,21 @@
 package tw.eeits.unhappy.gy.order.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 import tw.eeits.unhappy.gy.domain.Order;
 
+import java.util.List;
+import java.util.Optional;
+
+@Repository
 public interface OrderRepository extends JpaRepository<Order, Integer> {
+    // 解決 LAZY loading (查明細)
+    @EntityGraph(attributePaths = "orderItems")
+    Optional<Order> findWithItemsById(@Param("id") Integer id);
+
+    // 查會員所有訂單 (依時間排序，從最新到最舊)
+    List<Order> findByUserMember_IdOrderByCreatedAtDesc(Integer userId);
 }
