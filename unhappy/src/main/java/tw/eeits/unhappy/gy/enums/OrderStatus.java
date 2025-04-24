009f885d6dd03501fc0c8b865365763d438f2ed5
David227-MSI 😎 <a2272485@gmail.com>
2025-04-24 18:54:10 +0800

    feat(order): 新增 OrderStatus 和 PaymentStatus 列舉，定義訂單及付款狀態

---
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/enums/OrderStatus.java
@@ -0,0 +1,18 @@
+package tw.eeits.unhappy.gy.enums;
+
+public enum OrderStatus {
+    PENDING("待付款"),
+    PAID("已付款"),
+    COMPLETED("已完成"),
+    CANCELED("取消");
+
+    private final String displayText;
+
+    OrderStatus(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+}
---
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/enums/PaymentStatus.java
@@ -0,0 +1,16 @@
+package tw.eeits.unhappy.gy.enums;
+
+public enum PaymentStatus {
+    UNPAID("未付款"),
+    PAID("已付款"),
+    FAILED("付款失敗");
+    private final String displayText;
+
+    PaymentStatus(String displayText) {
+        this.displayText = displayText;
+    }
+
+    public String getDisplayText() {
+        return displayText;
+    }
+}
