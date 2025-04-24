3fbaf8326b1e5e0167badaa853780025505cf50a
David227-MSI 😎 <a2272485@gmail.com>
2025-04-24 22:39:38 +0800

    feat(dto): 新增 OrderDetailResponseDTO 和 OrderItemResponseDTO 類別以擴充訂單回應資料

---
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/dto/OrderItemResponseDTO.java
@@ -0,0 +1,20 @@
+package tw.eeits.unhappy.gy.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class OrderItemResponseDTO {
+    private Integer productId;
+    private String productName;
+    private Integer quantity;
+    private BigDecimal unitPrice;
+    private BigDecimal subtotal;
+}
