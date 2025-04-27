ee99f5fe35838adf4bc1321c927526fa4c17aa31
David227-MSI 😎 <a2272485@gmail.com>
2025-04-27 23:20:38 +0800

    feat(order): enhance OrderRequestDTO with recipient details and item list

--- unhappy/src/main/java/tw/eeits/unhappy/gy/dto/OrderRequestDTO.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/dto/OrderRequestDTO.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class OrderRequestDTO {
     private Integer userId;
-    private String couponPublishedId;
+    private String recipientName;
+    private String recipientPhone;
+    private String recipientAddress;
+    private List<OrderItemRequestDTO> items; // 購買商品明細
+    private String couponPublishedId; // 優惠券可以為空
 }
