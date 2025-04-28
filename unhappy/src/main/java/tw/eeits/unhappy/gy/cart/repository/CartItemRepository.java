841d0fa8931035d42d0f9393e87a7acb2a6bd913
David227-MSI 😎 <a2272485@gmail.com>
2025-04-28 13:32:34 +0800

    feat(cart): add method to mark cart item as checked out

--- unhappy/src/main/java/tw/eeits/unhappy/gy/cart/repository/CartItemRepository.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/cart/repository/CartItemRepository.java
@@ -1,6 +1,9 @@
 package tw.eeits.unhappy.gy.cart.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import tw.eeits.unhappy.gy.domain.CartItem;
 
@@ -21,6 +24,11 @@ public interface CartItemRepository extends JpaRepository<CartItem, Integer> {
 
     //清空使用者購物車
     void deleteByUserMember_IdAndCheckedOutFalse(Integer userId);
+
+    // 更新購物車商品為已結帳狀態
+    @Modifying
+    @Query("UPDATE CartItem c SET c.checkedOut = true, c.updatedAt = CURRENT_TIMESTAMP WHERE c.id = :cartItemId")
+    void markCartItemCheckedOutById(@Param("cartItemId") Integer cartItemId);
 
     //判斷是否存在(只判斷是否，效率高)
     boolean existsByUserMember_IdAndProduct_IdAndCheckedOutFalse(Integer userId, Integer productId);
