50262ca930f739b81e01d4eb88203b349e922c6f
David227-MSI 😎 <a2272485@gmail.com>
2025-04-28 09:55:03 +0800

    chore: remove sql files from tracking

--- sql_tables/9_remove_database.sql
+++
@@ -1,8 +0,0 @@
--- remove database ---------------------------------------------------------------------------
-USE master;
-IF DB_ID('ProjectDB') IS NOT NULL
-BEGIN
-    ALTER DATABASE ProjectDB SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
-    DROP DATABASE ProjectDB;
-    PRINT 'Database ProjectDB dropped successfully';
-END
