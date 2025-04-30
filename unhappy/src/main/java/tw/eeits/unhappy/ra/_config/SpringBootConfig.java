e9d781c9e3eadd99bdb86b91fa1bd8d4de4f6426
Raina <hibari1124@gmail.com>
2025-04-30 19:49:14 +0800

    註解掉自己的config

--- unhappy/src/main/java/tw/eeits/unhappy/ra/_config/SpringBootConfig.java
+++ unhappy/src/main/java/tw/eeits/unhappy/ra/_config/SpringBootConfig.java
@@ -1,28 +1,28 @@
-package tw.eeits.unhappy.ra._config;
+// package tw.eeits.unhappy.ra._config;
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+// import org.springframework.context.annotation.Configuration;
+// import org.springframework.web.servlet.config.annotation.CorsRegistry;
+// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class SpringBootConfig implements WebMvcConfigurer {
+// @Configuration
+// public class SpringBootConfig implements WebMvcConfigurer {
 
-	// 如果有CORS問題在下面添加路徑白名單
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        String[] allowedPaths = {
-            "/api/media/**",
-            "/api/reviews/**"
-            // 允許路徑貼在這
-        };
+// 	// 如果有CORS問題在下面添加路徑白名單
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+//         String[] allowedPaths = {
+//             "/api/media/**",
+//             "/api/reviews/**"
+//             // 允許路徑貼在這
+//         };
 
-        for (String path : allowedPaths) {
-            registry.addMapping(path)
-                    .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
-                    .allowedOrigins("*")
-                    .allowedHeaders("*")
-                    .allowCredentials(false);
-        }
-    }
-}
+//         for (String path : allowedPaths) {
+//             registry.addMapping(path)
+//                     .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+//                     .allowedOrigins("*")
+//                     .allowedHeaders("*")
+//                     .allowCredentials(false);
+//         }
+//     }
+// }
 
