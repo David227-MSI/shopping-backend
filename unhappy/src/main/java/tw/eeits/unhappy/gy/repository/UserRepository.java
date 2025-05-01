8fb6dedf32a5a46dc42a0a8cec27c9f4f2783b96
David227-MSI 😎 <a2272485@gmail.com>
2025-05-01 22:18:31 +0800

    chore: remove unused

--- unhappy/src/main/java/tw/eeits/unhappy/gy/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-// package tw.eeits.unhappy.gy.repository;
-
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.stereotype.Repository;
-// import tw.eeits.unhappy.gy.domain.UserMember;
-
-// @Repository
-// public interface UserRepository extends JpaRepository<UserMember, Integer> {
-// }
