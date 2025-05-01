d35acc27cb4b745611f590df0e6d23495b32ada3
David227-MSI 😎 <a2272485@gmail.com>
2025-05-01 22:18:48 +0800

    refactor(ecpay): change generateEcpayForm method to return a Map

--- unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/service/EcpayService.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/service/EcpayService.java
@@ -3,7 +3,9 @@ package tw.eeits.unhappy.gy.payment.ecpay.service;
 import jakarta.servlet.http.HttpServletRequest;
 import tw.eeits.unhappy.gy.dto.PaymentRequestDTO;
 
+import java.util.Map;
+
 public interface EcpayService {
-    String generateEcpayForm(PaymentRequestDTO paymentRequestDTO);
+    Map<String, String> generateEcpayForm(PaymentRequestDTO paymentRequestDTO);
     String handlePaymentCallback(HttpServletRequest request);
 }
