e5364a93efc9a305eaa18c8eda4fbc4b93f9e3a5
David227-MSI 😎 <a2272485@gmail.com>
2025-05-01 22:18:39 +0800

    refactor(ecpay): update startPayment method to return a Map and add logging for payment callback

--- unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/controller/EcpayController.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/controller/EcpayController.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 import tw.eeits.unhappy.gy.dto.PaymentRequestDTO;
 import tw.eeits.unhappy.gy.payment.ecpay.service.EcpayService;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/ecpay")
 public class EcpayController {
@@ -19,14 +21,15 @@ public class EcpayController {
 
     // 前端呼叫用: 產生綠界表單
     @PostMapping("/start-payment")
-    public ResponseEntity<String> startPayment(@RequestBody PaymentRequestDTO paymentRequestDTO) {
-        String paymentForm = ecpayService.generateEcpayForm(paymentRequestDTO);
+    public ResponseEntity<Map<String, String>>  startPayment(@RequestBody PaymentRequestDTO paymentRequestDTO) {
+        Map<String, String> paymentForm = ecpayService.generateEcpayForm(paymentRequestDTO);
         return ResponseEntity.ok(paymentForm);
     }
 
     // 綠界呼叫: 付款完成通知
     @PostMapping("/payment-callback")
     public ResponseEntity<String> paymentCallback(HttpServletRequest request) {
+        System.out.println("✅ 進入綠界付款 callback");
         String result = ecpayService.handlePaymentCallback(request);
         return ResponseEntity.ok(result);
     }
