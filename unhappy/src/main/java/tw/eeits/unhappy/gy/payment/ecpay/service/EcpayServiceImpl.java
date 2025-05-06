7f72b7d8bc0c8906306639785be8582f6d35c243
David227-MSI 😎 <a2272485@gmail.com>
2025-05-06 17:55:58 +0800

    refactor(EcpayServiceImpl): update Ecpay parameters for improved security

--- unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/service/EcpayServiceImpl.java
+++ unhappy/src/main/java/tw/eeits/unhappy/gy/payment/ecpay/service/EcpayServiceImpl.java
@@ -38,9 +38,12 @@ public class EcpayServiceImpl implements EcpayService {
 
 
     // 綠界參數
-    private final String HASH_KEY = "pwFHCqoQZGmho4w6";
-    private final String HASH_IV = "EkRm7iFT261dpevs";
-    private final String MERCHANT_ID = "3002607";
+//    private final String HASH_KEY = "pwFHCqoQZGmho4w6";
+//    private final String HASH_IV = "EkRm7iFT261dpevs";
+//    private final String MERCHANT_ID = "3002607";
+    private final String HASH_KEY = "5294y06JbISpM5x9";
+    private final String HASH_IV = "v77hoKGq4KWxNNIS";
+    private final String MERCHANT_ID = "2000132";
 
     @Override
     public Map<String, String> generateEcpayForm(PaymentRequestDTO dto) {
